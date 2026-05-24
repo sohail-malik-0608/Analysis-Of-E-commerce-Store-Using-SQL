5e8eca3c7a0da242ec6847bd21de8b3846690903
sohail-malik-0608 <sohailm822@gmail.com>
2026-05-24 18:10:33 +0530

    Add files via upload

---
+++ SQL Files/insert_regions.sql
@@ -0,0 +1,11 @@
+INSERT INTO Regions (RegionID, RegionName, Country) VALUES
+(1, 'North America', 'USA'),
+(2, 'South America', 'Brazil'),
+(3, 'Europe West', 'UK'),
+(4, 'Europe Central', 'Germany'),
+(5, 'Asia East', 'Japan'),
+(6, 'Asia South', 'India'),
+(7, 'Oceania', 'Australia'),
+(8, 'Africa North', 'Egypt'),
+(9, 'Africa South', 'South Africa'),
+(10, 'Middle East', 'UAE');
